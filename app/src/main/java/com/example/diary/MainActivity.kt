1c0f55e7baa4c689cce96dcbcc3ec3002f396cc4
ygreee <ygreee0320@gmail.com>
2023-07-17 17:10:57 +0900

    메뉴바 추가

--- app/src/main/java/com/example/diary/MainActivity.kt
+++ app/src/main/java/com/example/diary/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.diary.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.internal.NavigationMenuItemView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //bottom navigator
+        binding.bottomMenu.setOnItemSelectedListener {item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.home -> HomeFragment()
+
+                    R.id.map -> MapFragment()
+
+                    R.id.plan -> PlanFragment()
+
+                    R.id.ranking -> RankingFragment()
+
+                    else -> DiaryFragment()
+                }
+            )
+            true
+        }
+
+        // 초기화면으로 HomeFragment를 보여줄 수 있도록 설정
+        changeFragment(HomeFragment())
+    }
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container,fragment)
+            .commit()
     }
 }
