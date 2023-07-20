ab6c44512c858b94d56554cd3cf2d3c9436f11d9
ygreee <ygreee0320@gmail.com>
2023-07-21 00:26:05 +0900

    툴바 추가

--- app/src/main/java/com/example/diary/MainActivity.kt
+++ app/src/main/java/com/example/diary/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.diary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.text.TextUtils.replace
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.example.diary.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -21,18 +25,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
         //bottom navigator
         binding.bottomMenu.setOnItemSelectedListener {item ->
             changeFragment(
                 when (item.itemId) {
                     R.id.home -> HomeFragment()
-
                     R.id.map -> MapFragment()
-
                     R.id.plan -> PlanFragment()
-
                     R.id.ranking -> RankingFragment()
-
                     else -> DiaryFragment()
                 }
             )
@@ -47,5 +49,24 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.fragment_container,fragment)
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)  //menu_actionbar 불러 와서 사용할 것임, 연결 (menu 와)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.toolbar_nav) { // 툴바의 toolbar_nav 버튼 클릭 시
+            if (binding.drawer.isDrawerOpen(GravityCompat.END)) {
+                binding.drawer.closeDrawer(GravityCompat.END) // 드로어 레이아웃 닫기
+            } else {
+                binding.drawer.openDrawer(GravityCompat.END) // 드로어 레이아웃 열기
+            }
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
