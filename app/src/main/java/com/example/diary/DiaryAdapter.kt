b942dd0c4069839a2b535536f9a5aced4c46c56d
이규민 <90364652+20210815@users.noreply.github.com>
2023-09-20 15:18:26 +0900

    일기 대표 이미지 s3로 수정

--- app/src/main/java/com/example/diary/DiaryAdapter.kt
+++ app/src/main/java/com/example/diary/DiaryAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.diary
 
 import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,19 +12,25 @@ import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
+import com.amazonaws.mobileconnectors.s3.transferutility.*
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
 import com.bumptech.glide.Glide.init
+import com.example.diary.databinding.DiaryDetailPlaceRecyclerviewBinding
+import com.example.diary.databinding.DiaryRecyclerviewBinding
+import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
 
 class DiaryAdapter(private var diaries: List<DiaryDetailResponse>) : RecyclerView.Adapter<DiaryAdapter.DiaryViewHolder>()  {
     private var searchDiary = false // 일기 검색이라면 true, 내 일기 목록이라면 false
-    private lateinit var transferUtility: TransferUtility
 
+    // Amazon S3 관련 설정
+    private val awsAccessKey = "1807222EE827BB41A77C"
+    private val awsSecretKey = "E9DC72D2C24094CB2FE00763EF33330FB7948154"
+    private val awsCredentials = BasicAWSCredentials(awsAccessKey, awsSecretKey)
+    val s3Client = AmazonS3Client(awsCredentials, Region.getRegion(Regions.AP_NORTHEAST_2))// YOUR_REGION을 원하는 지역으로 변경하세요
+    private lateinit var transferUtility: TransferUtility
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DiaryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.diary_recyclerview, parent, false)
         return DiaryViewHolder(view)
@@ -85,11 +93,37 @@ class DiaryAdapter(private var diaries: List<DiaryDetailResponse>) : RecyclerVie
             placeTextView.text = diary.diaryDto.travelDest
             if (diary.diaryDto.imageUri != null) {
                 diaryImg.setImageURI(diary.diaryDto.imageUri.toUri())
+                s3Client.setEndpoint("https://kr.object.ncloudstorage.com")
+                // Initialize TransferUtility
+                TransferNetworkLossHandler.getInstance(diaryImg.context);
+
+                transferUtility = TransferUtility.builder()
+                    .s3Client(s3Client)
+                    .context(diaryImg.context)
+                    .defaultBucket("diary") // S3 버킷 이름을 변경하세요
+                    .build()
+                if (diary.diaryDto.imageUri != null) {
+                    // 이미지를 여러 개 표시하기 위해 RecyclerView로 변경
+                    Log.d("diaryDetailAdapter", ""+diary.diaryDto.imageUri)
+
+                    downloadAndInitializeAdapter(diary.diaryDto.imageUri.toUri(), diaryImg)
+//            val imageAdapter = MultiImageAdapter(uriList as ArrayList<Uri>, holder.binding.root.context)
+//            holder.binding.recyclerView.adapter = imageAdapter
+//            holder.binding.recyclerView.layoutManager = layoutManager
+                    Log.d("detailAdapter", "이미지 추가")
+                } else {
+                    // 이미지가 없는 경우, RecyclerView를 숨깁니다.
+                    Log.d("detailAdapter", "이미지 없음")
+                }
             }
 
 
 
-            if (searchDiary) { // 일기 검색 목록이라면
+
+
+
+
+        if (searchDiary) { // 일기 검색 목록이라면
                 diaryInfoLayout.visibility = View.VISIBLE
                 periodTextView.visibility = View.GONE
 
@@ -109,4 +143,41 @@ class DiaryAdapter(private var diaries: List<DiaryDetailResponse>) : RecyclerVie
             commentView.text = "${diary.diaryDto.comments.size}"
         }
     }
-}
+
+    private fun downloadAndInitializeAdapter(imageUri: Uri, binding: ImageView) {
+        // 다운로드할 파일 경로
+        //val downloadFile = File(binding.root.context.cacheDir, "downloaded_image")
+
+        //val transferObserverList = mutableListOf<TransferObserver>()
+
+        //val downloadFiles = ArrayList<Uri>() // 이미지 다운로드를 위한 파일 리스트
+
+        val fileName = imageUri.lastPathSegment // 파일 이름을 가져옴
+        val downloadFile = File(binding.context.cacheDir, fileName)
+        //downloadFiles.add(downloadFile.toUri()) // 파일을 리스트에 추가
+        val transferObserver = transferUtility.download(
+            "diary",
+            imageUri.toString(),
+            downloadFile
+        )
+
+        // 모든 이미지 다운로드 완료를 기다림
+        val completionCount = AtomicInteger(0)
+
+        transferObserver.setTransferListener(object : TransferListener {
+                override fun onStateChanged(id: Int, state: TransferState) {
+                    if (state == TransferState.COMPLETED) {
+                        completionCount.incrementAndGet()
+                    }
+                }
+
+                override fun onProgressChanged(id: Int, bytesCurrent: Long, bytesTotal: Long) {
+                    // 진행 상태 업데이트
+                }
+
+                override fun onError(id: Int, ex: Exception) {
+                    Log.e("DiaryDetailAdapter", "이미지 다운로드 오류: $ex")
+                }
+            })
+        }
+    }
