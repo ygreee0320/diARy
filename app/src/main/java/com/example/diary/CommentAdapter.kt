d8d7863b98376b46eb71858162c8cfbe72dec8ab
이규민 <90364652+20210815@users.noreply.github.com>
2023-09-21 15:19:32 +0900

    댓글 날짜 형식 변경

--- app/src/main/java/com/example/diary/CommentAdapter.kt
+++ app/src/main/java/com/example/diary/CommentAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.diary
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -40,9 +42,37 @@ class CommentAdapter(private var comments: List<CommentListResponse>) : Recycler
             usernameTextView.text = comment.userDto.username
 
             // 댓글 날짜 시간도 출력 수정 필요(현재 날짜만 보임)
-            val outputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-            val formattedDate = outputFormat.format(comment.createdAt)
-            createdTextView.text = formattedDate
+            val parts = comment.createdAt.split("T")
+
+            if (parts.size == 2) {
+                val datePart = parts[0]
+                val timeWithMillisPart = parts[1]
+
+                // 밀리초 부분을 제외한 시간 부분 추출
+                val timePart = timeWithMillisPart.substring(0, 8)
+
+                // 날짜와 시간을 조합하여 Timestamp로 변환
+                val timestampString = "$datePart $timePart"
+                val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+                val parsedTimestamp = Timestamp(dateFormat.parse(timestampString).time)
+
+                // SimpleDateFormat을 사용하여 원하는 형식으로 포맷
+                val outputDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+                outputDateFormat.timeZone = TimeZone.getTimeZone("Asia/Seoul") // 원하는 시간대 설정
+                val formattedDate = outputDateFormat.format(parsedTimestamp)
+
+                // formattedDate를 TextView에 설정
+                createdTextView.text = formattedDate
+                // 결과를 출력
+                Log.d("Formatted Date", formattedDate)
+            } else {
+                // 올바른 형식이 아닐 경우 오류 처리
+                Log.e("Error", "Invalid timestamp format")
+            }
+//
+//            val outputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+//            val formattedDate = outputFormat.format(comment.createdAt)
+//            createdTextView.text = formattedDate
         }
     }
 }
